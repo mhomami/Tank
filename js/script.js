f511f204760a124abe93d721e8d52efadf64f177
mhomami <shahram.homami@gmail.com>
2021-10-12 19:21:34 -0700

    organise img files/condense code

--- js/script.js
+++ js/script.js
@@ -12,7 +12,6 @@ const renderer = new PIXI.Renderer({
   autoDensity: true
 });
 
-
 window.addEventListener('resize', resize);
 
 function resize(){
@@ -22,94 +21,82 @@ function resize(){
 }
 
 function randomY(){
-  return Math.floor(Math.random()*_h);
+  return Math.floor(Math.random() * _h);
 }
 
-function newSprite(spriteName, spriteFile){
-  return spriteName = new PIXI.Sprite(spriteFile);
+//function to condense driver/jimmy initial setup
+function makeSprite(spriteFile){
+  const tSpr = stage.addChild(new PIXI.Sprite(spriteFile));
+  return tSpr;
 }
 
 const stage = new PIXI.Container();
-const driver1 = PIXI.Texture.from('driver_pix3x.png');
-const driver2 = PIXI.Texture.from('driver_pix_inv3x.png');
-const jimmy1 = PIXI.Texture.from('jimmy1x_pix.png');
+const driver1 = PIXI.Texture.from('/img/driver_pix3x.png');
+const driver2 = PIXI.Texture.from('/img/driver_pix_inv3x.png');
+const jimmy1 = PIXI.Texture.from('/img/jimmy1x_pix.png');
 
-const bitsImages = ["bits_pix3x1.png", "bits_pix3x2.png", "bits_pix3x3.png", "bits_pix3x4.png",
-"bits_pix3x5.png","bits_pix3x6.png","bits_pix3x7.png","bits_pix3x8.png","bits_pix3x9.png",
-"bits_pix3x10.png","bits_pix3x11.png"];
-
+// animate bits
+const bitsImages = ["/img/bits_pix3x1.png", "/img/bits_pix3x2.png", "/img/bits_pix3x3.png", "/img/bits_pix3x4.png",
+"/img/bits_pix3x5.png","/img/bits_pix3x6.png","/img/bits_pix3x7.png","/img/bits_pix3x8.png","/img/bits_pix3x9.png",
+"/img/bits_pix3x10.png","/img/bits_pix3x11.png"];
 const textureArray = bitsImages.map( image => PIXI.Texture.from(image));
 
-let driver1sprite, driver2sprite, jimmy1sprite, bits1sprite, bits2sprite, bits3sprite, bits4sprite;
-
-/* PIXI.Loader.shared.add("../bits_pix3x.json").load(setup);
-
-function setup(){
-  let bitSprite = PIXI.Loader.shared.resources["../bits_pix3x.json"].spritesheet;
-  animatedSprite = new PIXI.AnimatedSprite(sheet.animations["image_sequence"]);
-} */
-
-// driver1sprite = stage.addChild(new PIXI.Sprite(driver1));
-
-driver1sprite = newSprite(driver1sprite, driver1);
+// initial setup for drivers and jimmy
+// x and y get overwritten in the animate function once fish are reset when they drift offscreen
+const driver1sprite = makeSprite(driver1);
 driver1sprite.x = _w + 600;
-
-// driver1sprite = new PIXI.Sprite(driver1);
-// driver1sprite.x = _w + 600;
-// stage.addChild(driver1sprite);
-
-driver2sprite = new PIXI.Sprite(driver2);
+const driver2sprite = makeSprite(driver2);
 driver2sprite.x = _w - (_w + 400);
-stage.addChild(driver2sprite);
-
-jimmy1sprite = new PIXI.Sprite(jimmy1);
+const jimmy1sprite = makeSprite(jimmy1);
 jimmy1sprite.x = _w + 100;
-stage.addChild(jimmy1sprite);
 
-bits1sprite = new PIXI.AnimatedSprite(textureArray);
+///////////////////////////// START - find a way to condense this //////////////////////////////////////
+const bits1sprite = stage.addChild(new PIXI.AnimatedSprite(textureArray));
 bits1sprite.x = _w - (_w + 1550);
 bits1sprite.y = 200;
 bits1sprite.animationSpeed = 0.2;
 bits1sprite.play();
-stage.addChild(bits1sprite);
 
-bits2sprite = new PIXI.AnimatedSprite(textureArray);
+const bits2sprite = stage.addChild(new PIXI.AnimatedSprite(textureArray));
 bits2sprite.x = _w - (_w + 1450);
 bits2sprite.animationSpeed = 0.2;
 bits2sprite.y = 300;
 bits2sprite.play();
-stage.addChild(bits2sprite);
 
-bits3sprite = new PIXI.AnimatedSprite(textureArray);
+const bits3sprite = stage.addChild(new PIXI.AnimatedSprite(textureArray));
 bits3sprite.x = _w - (_w + 1250);
 bits3sprite.animationSpeed = 0.2;
 bits3sprite.y = 150;
 bits3sprite.play();
-stage.addChild(bits3sprite);
 
-bits4sprite = new PIXI.AnimatedSprite(textureArray);
+const bits4sprite = stage.addChild(new PIXI.AnimatedSprite(textureArray));
 bits4sprite.x = _w - (_w + 1700);
 bits4sprite.animationSpeed = 0.2;
 bits4sprite.y = 250;
 bits4sprite.play();
-stage.addChild(bits4sprite);
+///////////////////////////// END - find a way to condense this /////////////////////////////////////////
 
 const ticker = new PIXI.Ticker();
 ticker.add(animate);
 ticker.start();
 
 let delta = 0;
+let y1, y2, y3;
+
 y1 = randomY();
-y2 = Math.floor(Math.random() * _h);
-y3 = Math.floor(Math.random() * _h);
+y2 = randomY();
+y3 = randomY();
+
+// y2 = Math.floor(Math.random() * _h);
+// y3 = Math.floor(Math.random() * _h);
 
 function animate(){
   delta += 0.025;
   rndInt = Math.floor(Math.random() * _h);
+  // rndInt = randomY();
   
   driver1sprite.y = y1 + Math.sin(delta) * 5;
   driver2sprite.y = y2 + Math.sin(delta + 1.5) * 5;
-
   jimmy1sprite.y = y3 + Math.cos(delta) * 5;
 
   renderer.render(stage);
